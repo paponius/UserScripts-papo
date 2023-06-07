a6ee7d4e3c3030d52c196c124d47f772e7fb2be3
paponius <palo.hmm@gmail.com>
2023-06-07 12:38:30 +0200

    Create tandt.posta.sk.user.js

---
+++ GM/tandt.posta.sk.user.js
@@ -0,0 +1,27 @@
+// This is a GreaseMonkey user script file.
+// It contains only the Metadata Block.
+// All JavaScript code is in separate files, to make development more portable and easier.
+//
+// JS files included using require Key are downloaded only once while version Key here is the same.
+// Grease Monkey (and alternatives) will download and cache all included files, joined all in one.
+// There is no speed or bandwidth use difference to a method where all JS code is written in this one file.
+// And the same is with transparency. Included external files can be inspected in GM (or Tamper Monkey) editor in separate tabs.
+
+// ==UserScript==
+// @name         tandt.posta.sk
+// @namespace    http://www.github.com/tandt.posta.sk/
+// @description  Changes plain text with link on tandt.posta.sk page.
+// @author       papo
+// @version      1.0.0
+// @license      GPLv2
+
+// @match          *://tandt.posta.sk/
+// @match          *://www.posta.sk/*
+
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=posta.sk
+// @grant none
+
+// @require      https://github.com/paponius/papo-userscripts/raw/master/src/tandt.posta.sk.js
+
+// @connect      *
+// ==/UserScript==
--- README.md
+++ README.md
@@ -10,3 +10,7 @@ MutationObserver
 
 Handling an iframe. With JS injections in both top and iframe windows
 and communication between them.
+
+
+### Install in GM
+https://github.com/paponius/papo-userscripts/raw/master/GM/tandt.posta.sk.user.js
